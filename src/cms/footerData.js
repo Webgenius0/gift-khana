677b57c456137cba2e2c348effa488fb842ecd2e
ArunRoy404 <arunroy0065@gmail.com>
2026-02-11 12:47:54 +0600

    feat: footer created

---
+++ src/cms/footerData.js
@@ -0,0 +1,17 @@
+export const footerData = {
+    links: [
+        { label: "My Account", href: "#" },
+        { label: "Track my order", href: "#" },
+        { label: "Return Policy", href: "#" },
+        { label: "About us", href: "#" }
+    ],
+    contact: {
+        phone: "+201225722929"
+    },
+    help: {
+        text: "Get Help",
+        href: "#"
+    },
+    paymentImage: "/payment.png",
+    copyright: "Gift Khana Inc. All rights reserved. User Agreement | Privacy Notice | Community Guidelines | Accessibility ,2025-2000 © Copyright"
+};
--- src/templates/HomePage.jsx
+++ src/templates/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/components/Shared/NavBar/Header';
 import Banner from '@/components/HomePageComponents/Banner';
+import Footer from '@/components/Shared/Footer/Footer';
 
 const HomePage = () => {
     return (
@@ -10,6 +11,7 @@ const HomePage = () => {
                 <Header />
             </div>
             <Banner />
+            <Footer />
         </div>
     );
 };
