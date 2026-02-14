708a9ebd9179117757115f69e337cde6de847bca
Arun Roy <arunroy0065@gmail.com>
2026-02-14 12:28:03 +0600

    feat: hover effect added to button

--- src/cms/ctaSectionData.js
+++ src/cms/ctaSectionData.js
@@ -4,21 +4,35 @@ import button3 from "@/assets/button_images/button3_image.png";
 import button4 from "@/assets/button_images/button4_image.png";
 
 
+import element1 from "@/assets/button_images/button1_pop_element.png";
+import element2 from "@/assets/button_images/button2_pop_element.png";
+import element3 from "@/assets/button_images/button3_pop_element.png";
+import element4 from "@/assets/button_images/button4_pop_element.png";
+
+
 export const ctaSectionData = [
     {
         title: "Create you own",
         buttonImage: button1,
+        element: element1,
+        classNameElement: "absolute scale-35 group-hover:scale-55 smooth max-w-max max-h-max",
     },
     {
         title: "Home decor",
         buttonImage: button2,
+        element: element2,
+        classNameElement: "absolute -top-30 right-6 scale-37 group-hover:scale-45 group-hover:-top-35 smooth",
     },
     {
         title: "Gadgets & Accessories",
         buttonImage: button3,
+        element: element3,
+        classNameElement: "absolute -top-33 -left-2 scale-35 group-hover:-top-45 smooth",
     },
     {
         title: "Ready-made",
         buttonImage: button4,
+        element: element4,
+        classNameElement: "absolute -top-35 -right-21 scale-45 group-hover:scale-55 group-hover:-top-40 smooth",
     }
 ];
