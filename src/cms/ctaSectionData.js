6120bcd489220be7a100545a6e5ee59c4853f17e
Arun Roy <arunroy0065@gmail.com>
2026-02-14 14:18:26 +0600

    refactor: button hover effect made responsive

--- src/cms/ctaSectionData.js
+++ src/cms/ctaSectionData.js
@@ -21,18 +21,21 @@ export const ctaSectionData = [
         title: "Home decor",
         buttonImage: button2,
         element: element2,
-        classNameElement: "absolute -top-30 right-6 scale-37 group-hover:scale-45 group-hover:-top-35 smooth",
+        classNameElement: "absolute top-0 right-20 md:right-15 lg:right-30 w-[25%] group-hover:w-[30%] group-hover:right-18 group-hover:md:right-13 group-hover:lg:right-25 group-hover:-top-3 group-hover:md:-top-5 group-hover:lg:-top-7 smooth",
+        // classNameElement: "absolute -top-30 right-6 scale-37 group-hover:scale-45 group-hover:-top-35 smooth",
     },
     {
         title: "Gadgets & Accessories",
         buttonImage: button3,
         element: element3,
-        classNameElement: "absolute -top-33 -left-2 scale-35 group-hover:-top-45 smooth",
+        classNameElement: "absolute top-0 left-[6%] w-[10%] group-hover:-top-10 smooth",
+        // classNameElement: "absolute -top-33 -left-2 scale-35 group-hover:-top-45 smooth",
     },
     {
         title: "Ready-made",
         buttonImage: button4,
         element: element4,
-        classNameElement: "absolute -top-35 -right-21 scale-45 group-hover:scale-55 group-hover:-top-40 smooth",
+        classNameElement: "absolute top-0 right-5 md:right-5 lg:right-10 w-[35%] group-hover:w-[43%] group-hover:-top-10 group-hover:right-1 group-hover:md:right-2 group-hover:lg:right-5 group-hover:-top-3 group-hover:md:-top-5 group-hover:lg:-top-15 smooth",
+        // classNameElement: "absolute -top-35 -right-21 scale-45 group-hover:scale-55 group-hover:-top-40 smooth",
     }
 ];
