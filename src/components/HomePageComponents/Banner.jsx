ecfaa3ad3cdf8a06cff8e133ec1c5f9c53a0fd56
Arun Roy <arunroy0065@gmail.com>
2026-02-17 11:30:08 +0600

    feat: backdrop blur added

--- src/components/HomePageComponents/Banner.jsx
+++ src/components/HomePageComponents/Banner.jsx
@@ -10,7 +10,7 @@ export default function Banner() {
     return (
         <section className="relative w-full lg:aspect-video overflow-hidden max-w-[1920px] mx-auto">
             {/* Background Media Layer */}
-            <div className="lg:absolute inset-0 z-0">
+            <div className="lg:absolute inset-0 z-0  -mb-2">
                 {/* Video - Plays once */}
                 <video
                     autoPlay
@@ -30,28 +30,29 @@ export default function Banner() {
                 /> */}
 
             </div>
+            <div className="bg-bg-primary lg:bg-transparent w-full z-1 relative">
+                {/* Content Layer */}
+                <CommonContainer className="relative z-10 h-full w-full">
+                    <div className="h-full mx-auto lg:mx-0 flex flex-col justify-start lg:pt-46 max-w-2xl">
+                        {/* Title */}
+                        <h1 className="text-center lg:text-left text-3xl lg:text-5xl text-secondary leading-tight font-bebas">
+                            {bannerData.title}
+                        </h1>
 
-            {/* Content Layer */}
-            <CommonContainer className="relative z-10 h-full w-full ">
-                <div className="h-full mx-auto lg:mx-0 flex flex-col justify-start lg:pt-46 max-w-2xl">
-                    {/* Title */}
-                    <h1 className="text-center lg:text-left text-3xl lg:text-5xl text-secondary leading-tight font-bebas">
-                        {bannerData.title}
-                    </h1>
+                        {/* Description */}
+                        <p className="relative text-center mx-auto lg:mx-0 lg:text-left text-xs md:text-lg font-medium text-secondary max-w-xl leading-relaxed lg:bg-white/20 lg:backdrop-blur-sm lg:px-4 py-3 rounded-xl">
+                            {bannerData.description}
+                        </p>
 
-                    {/* Description */}
-                    <p className="text-center mx-auto lg:mx-0 lg:text-left text-xs md:text-lg font-medium text-secondary/90 max-w-lg leading-relaxed">
-                        {bannerData.description}
-                    </p>
-
-                    {/* CTA Button from Figma */}
-                    <div className="mt-10 hidden lg:block">
-                        <Button className="px-8 py-6 text-lg shadow-lg">
-                            SHOP NOW
-                        </Button>
+                        {/* CTA Button from Figma */}
+                        <div className="mt-10 hidden lg:block">
+                            <Button className="px-8 py-6 text-lg shadow-lg">
+                                SHOP NOW
+                            </Button>
+                        </div>
                     </div>
-                </div>
-            </CommonContainer>
+                </CommonContainer>
+            </div>
         </section>
     );
 }
