c39bca16fde996e3b34a13f0f29f81345b93fd4a
Arun Roy <arunroy0065@gmail.com>
2026-02-19 10:59:24 +0600

    refactor: made 2 columns for the personalized products

--- src/templates/PersonalizedProductsPage.jsx
+++ src/templates/PersonalizedProductsPage.jsx
@@ -97,7 +97,7 @@ export default function PersonalizedProductsPage() {
                 </div>
 
                 {/* Product Grid */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
+                <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 md:gap-8">
                     {personalizedProductsData.map((product, index) => (
                         <ProductCard
                             key={product.id}
