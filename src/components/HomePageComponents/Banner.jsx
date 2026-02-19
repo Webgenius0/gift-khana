f666da1a456b4afa268666b47d05c46b348e0564
Arun Roy <arunroy0065@gmail.com>
2026-02-19 10:56:21 +0600

    refactor: backdrop blur added to the title and cta button

--- src/components/HomePageComponents/Banner.jsx
+++ src/components/HomePageComponents/Banner.jsx
@@ -18,21 +18,23 @@ export default function Banner() {
                 {/* Content Layer */}
                 <CommonContainer className="relative z-10 h-full w-full">
                     <div className="h-full mx-auto lg:mx-0 flex flex-col justify-start lg:pt-46 max-w-2xl">
-                        {/* Title */}
-                        <h1 className="text-center lg:text-left text-3xl lg:text-5xl text-secondary leading-tight font-bebas">
-                            {bannerData.title}
-                        </h1>
+                        <div className="lg:bg-white/20 lg:backdrop-blur-sm lg:px-4 py-3 rounded-xl">
+                            {/* Title */}
+                            <h1 className="text-center lg:text-left text-3xl lg:text-5xl text-secondary leading-tight font-bebas">
+                                {bannerData.title}
+                            </h1>
 
-                        {/* Description */}
-                        <p className="relative text-center mx-auto lg:mx-0 lg:text-left text-xs md:text-lg font-medium text-secondary max-w-xl leading-relaxed lg:bg-white/20 lg:backdrop-blur-sm lg:px-4 py-3 rounded-xl">
-                            {bannerData.description}
-                        </p>
+                            {/* Description */}
+                            <p className="relative text-center mx-auto lg:mx-0 lg:text-left text-xs md:text-lg font-medium text-secondary max-w-xl leading-relaxed ">
+                                {bannerData.description}
+                            </p>
 
-                        {/* CTA Button from Figma */}
-                        <div className="mt-10 hidden lg:block">
-                            <Button className="px-8 py-6 text-lg shadow-lg">
-                                SHOP NOW
-                            </Button>
+                            {/* CTA Button from Figma */}
+                            <div className="mt-10 hidden lg:block">
+                                <Button className="px-8 py-6 text-lg shadow-lg">
+                                    SHOP NOW
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </CommonContainer>
